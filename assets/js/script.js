926b96a40b3da45c106bfae6e2459c676cef8aeb
wafaj3fr <wafaagaafar@gmail.com>
2024-01-09 13:16:06 +0400

    fin

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const addEventOnElement = function(elem, type, callback){
+    if (elem.length > 1){
+        for (let i = 0; i < elem.length; i++){
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
+    }
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+}
+
+addEventOnElement(navTogglers, "click", toggleNavbar)
+
+const closeNavbar = function(){
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.classList.remove("active");
+}
+
+addEventOnElement(navbarLinks, "click", closeNavbar);
+
+const header = document.querySelector("[data-header]");
+const activeElemScroll = function(){
+    if (window.scrollY > 100){
+        header.classList.add("active");
+    }
+    else{
+        header.classList.remove("active");
+    }
+}
+
+addEventOnElement(window, "scroll", activeElemScroll)
